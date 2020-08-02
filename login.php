c9d97bb326843394775da7a0fedfbdbb68f3aa54
Rusty6113 <69097594+Rusty6113@users.noreply.github.com>
2020-08-01 22:56:39 -0400

    Add files via upload

---
+++ login.php
@@ -0,0 +1,351 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+  <meta http-equiv="Content-type" content="text/html; charset=utf-8">
+<meta name="viewport" content="user-scalable=no, initial-scale=1.0, maximum-scale=1.0, width=device-width">
+
+
+
+<link rel="canonical" href="https://www.pornhublive.com/login.php?langchoice=en">
+<link rel="apple-touch-icon-precomposed" href="/apple-touch-icon.png">
+
+<style type="text/css">
+  .ui-bar-s { background-color: #000000;}
+</style>
+
+<link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css" media="all">
+<link href='//fonts.googleapis.com/css?family=Open+Sans:400,600,700' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" type="text/css" href="//m2.nsimg.net/touch/shared/mobile/css/20160321/style.css" media="all">
+
+
+<script src="//m2.nsimg.net/static/x/jquery/jquery-1.7.min.js" integrity="sha384-dvd+ElNzr+uvccBW3+qM8CpqU18pcTpbzSpKdtVDTdbt2hZv+3/rLhjx73fqmRCa" crossorigin="anonymous"></script>
+<script src="//m2.nsimg.net/static/x/jquery/plugins/jquery.cookie.min.js" integrity="sha384-Lmavif5mtMmM7YCM8FzyX2sD6JM9gPi1qWNGzfQKr/fx/MUiTBTwWu91OeqdrPvx" crossorigin="anonymous"></script>
+
+<script type="text/javascript">
+  // This is used elsewhere to detect iPhone/iPod
+  var ua = navigator.userAgent.toLowerCase();
+
+  $(document).on("mobileinit", function () {
+    $.extend( $.mobile , {
+      ajaxEnabled: false,
+      activeBtnClass: 'hidden-jqm-active-button-class'
+    });
+  });
+</script>
+<script id="js-naiad" data-user-is="nonuser">
+  // Addresses an issue resulting from "Complitly" Chrome plugin/malware.
+  // stackoverflow.com/q/11501391
+  window.suggestmeyes_loaded = true;
+</script>
+
+
+<script src="//m2.nsimg.net/static/x/jquery/jquery.mobile-1.0.1.min.js" integrity="sha384-TWFl6pWjciYCOP4AEs0NsuTiw+NJL5RzE9JJE3SaeVG1Uw/SlBAwfihlfwy6NrOm" crossorigin="anonymous"></script>
+
+<script type="text/javascript">
+  var jqm = jQuery.noConflict();
+</script>
+
+
+
+        <!-- TP-GA code -->
+        <script>
+          
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		
+          ga('create', 'UA-118279723-1', 'auto');
+		  ga('set', 'page', '/login.php?AFNO=1-5457&g=L3NpZ251cC8%2FQUZOTz0xLTU0NTcmc3Rubz0yLTYzMC0wLTc2NzEtMC0wLTIwNzMtMjA1NQ%3D%3D&stno=2-630-0-7671-0-0-2073-2055');
+          ga('send', 'pageview', {'dimension1': 'Guest','dimension3': '5457','dimension4': ''});
+		          			
+      	        
+        ga('create', 'UA-8197088-49', 'auto', 'masterTracker');
+        ga('masterTracker.set', 'page', '/login.php?AFNO=1-5457&g=L3NpZ251cC8%2FQUZOTz0xLTU0NTcmc3Rubz0yLTYzMC0wLTc2NzEtMC0wLTIwNzMtMjA1NQ%3D%3D&stno=2-630-0-7671-0-0-2073-2055');
+        ga('masterTracker.send', 'pageview', {'dimension1': 'Guest','dimension3': '5457','dimension4': ''}); 
+		       
+      
+          
+        </script>
+      
+
+   <title>PornHubLive -  Members Login</title>
+</head>
+
+<body class="en">
+
+  <div data-role="page" id="login-page">
+
+    <header>
+  
+  <div class="ui-bar-s">
+
+    <a data-ajax="false" data-transition="pop" class="logo" href="https://www.pornhublive.com/?AFNO=1-5457&stno=2-630-0-7671-0-0-2073-2055">
+     <img src="//m1.nsimg.net/2.0/skin/phl/images/20150206/logo.png">
+    </a>
+
+    <div class="menu logged-out">
+      <a href="https://www.pornhublive.com/advanced-search.html?AFNO=1-5457&stno=2-630-0-7671-0-0-2073-2055" data-theme="a" data-role="button" data-iconpos="notext" data-inline="true" data-icon="search" data-shadow="false">Search</a>
+    </div>
+
+  </div>
+
+  <div class="ui-bar-c salutation">
+    <a href="https://pornhublive.com/signup/?stno=2-630-0-7671-0-0-2073-2055&AFNO=1-5457" rel="external" data-role="button" data-inline="true" data-theme="gold" data-shadow="false">Join Free</a>
+    <span>or</span>
+    <a href="https://www.pornhublive.com/login.php?AFNO=1-5457&g=L2xvZ2luLnBocD9BRk5PPTEtNTQ1NyZzdG5vPTItNjMwLTAtNzY3MS0wLTAtMjA3My0yMDU1&stno=2-630-0-7671-0-0-2073-2055" data-role="button" data-inline="true" data-theme="black" data-shadow="false">Login</a>
+  </div>
+
+</header>
+
+
+
+    <section data-role="content" data-theme="d">
+
+      <h3>Members Sign In</h3>
+    
+      
+      
+      <form name="loginform" data-ajax="false" action="https://www.pornhublive.com/login.php?AFNO=1-5457&g=L2xvZ2luLnBocD9BRk5PPTEtNTQ1NyZzdG5vPTItNjMwLTAtNzY3MS0wLTAtMjA3My0yMDU1&stno=2-630-0-7671-0-0-2073-2055" method="post" accept-charset="utf-8" class="login-form">
+
+        <input type="hidden" name="stno" value="2-630-0-7671-0-0-2073-2055"><input type="hidden" name="AFNO" value="1-5457">
+        <input type="hidden" name="trylogin" value="1">
+        <input type="hidden" name="tries" value="1">
+        <input type="hidden" name="g" value="L3NpZ251cC8/QUZOTz0xLTU0NTcmc3Rubz0yLTYzMC0wLTc2NzEtMC0wLTIwNzMtMjA1NQ==">
+        <input type="hidden" name="recognize" value="1" />
+        <INPUT TYPE="HIDDEN" NAME="stno" VALUE="2-630-0-7671-0-0-2073-2055">
+<INPUT TYPE="HIDDEN" NAME="AFNO" VALUE="1-5457">
+
+
+        <div data-role="fieldcontain">
+
+          <input type="email" name="sausr" value="" id="username" placeholder="Email"><br>
+
+          <input type="password" name="sapwd" value="" id="password" placeholder="Password"><br>
+
+          <button data-icon="arrow-r" data-iconpos="right" data-theme="black" type="submit">Login</button>
+        
+        </div>
+
+      </form>
+
+      <div class="forgot-section">
+
+  <form name="pwform" action="https://www.pornhublive.com/forgot-password/" 
+    method="post" id="forgot-password-form" rel="external" data-ajax="false">
+    <input type="hidden" name="stno" value="2-630-0-7671-0-0-2073-2055"><input type="hidden" name="AFNO" value="1-5457">
+    
+    <label for="email">Lost password <span>(enter your email address)</span></label>
+    <input class="text pwd" type="email" id="email" name="email" value="">
+    
+    <p><script src="https://www.google.com/recaptcha/api.js?hl=en"></script>
+<div class="g-recaptcha" data-sitekey="6LeljBoUAAAAAAgoCKzr4NX70PxsJKgTrTAM_dH5" data-theme="light"></div></p>
+    <button type="submit" class="button forgot-submit">Get password</button>
+
+  </form>
+
+</div>
+
+      
+      <p>Not a member yet? <a href="https://pornhublive.com/signup/?stno=2-630-0-7671-0-0-2073-2055&AFNO=1-5457">Join now</a></p>
+
+    </section>
+
+    <footer data-role="footer" data-theme="c">
+
+  <div data-role="navbar">
+    <ul>
+      <li><a rel="external" href="https://www.pornhublive.com/customer-service/?AFNO=1-5457&stno=2-630-0-7671-0-0-2073-2055">support</a></li>
+      <li><a rel="external" href="https://www.pornhublive.com/about-us.html?AFNO=1-5457&stno=2-630-0-7671-0-0-2073-2055">about</a></li>
+    </ul>
+  </div>
+
+  <div data-role="navbar">
+    <ul>
+      <li>
+        <label for="legal-select" class="ui-hidden-accessible">legal</label>
+        <select name="m-legal-select" id="m-legal-select" data-corners="false" data-autosubmit="true" >
+          <option value="" selected>legal</option>
+          <option value="https://www.pornhublive.com/2257.html?AFNO=1-5457&stno=2-630-0-7671-0-0-2073-2055">18 U.S.C. 2257 and 2257A Record-Keeping Requirements Compliance Statement</option>
+          <option value="https://www.pornhublive.com/dmca.html?AFNO=1-5457&stno=2-630-0-7671-0-0-2073-2055">DMCA Notice of Copyright Infringement</option>
+          <option value="https://www.pornhublive.com/terms.html?AFNO=1-5457&stno=2-630-0-7671-0-0-2073-2055">Terms &amp; Conditions</option>
+          <option value="https://www.pornhublive.com/privacy.html?AFNO=1-5457&stno=2-630-0-7671-0-0-2073-2055">Privacy Policy</option>
+          
+        </select>
+      </li>
+
+      <li>
+  <label for="lang-select" class="ui-hidden-accessible">language</label>
+  <select name="lang-select" id="lang-select" data-corners="false" data-autosubmit="true">
+    <option selected value="https://www.pornhublive.com/login.php?AFNO=1-5457&g=L3NpZ251cC8%2FQUZOTz0xLTU0NTcmc3Rubz0yLTYzMC0wLTc2NzEtMC0wLTIwNzMtMjA1NQ%3D%3D&langchoice=en&stno=2-630-0-7671-0-0-2073-2055">English</option><option value="https://www.pornhublive.com/login.php?AFNO=1-5457&g=L3NpZ251cC8%2FQUZOTz0xLTU0NTcmc3Rubz0yLTYzMC0wLTc2NzEtMC0wLTIwNzMtMjA1NQ%3D%3D&langchoice=de&stno=2-630-0-7671-0-0-2073-2055">Deutsch</option><option value="https://www.pornhublive.com/login.php?AFNO=1-5457&g=L3NpZ251cC8%2FQUZOTz0xLTU0NTcmc3Rubz0yLTYzMC0wLTc2NzEtMC0wLTIwNzMtMjA1NQ%3D%3D&langchoice=es&stno=2-630-0-7671-0-0-2073-2055">Español</option><option value="https://www.pornhublive.com/login.php?AFNO=1-5457&g=L3NpZ251cC8%2FQUZOTz0xLTU0NTcmc3Rubz0yLTYzMC0wLTc2NzEtMC0wLTIwNzMtMjA1NQ%3D%3D&langchoice=fr&stno=2-630-0-7671-0-0-2073-2055">Français</option><option value="https://www.pornhublive.com/login.php?AFNO=1-5457&g=L3NpZ251cC8%2FQUZOTz0xLTU0NTcmc3Rubz0yLTYzMC0wLTc2NzEtMC0wLTIwNzMtMjA1NQ%3D%3D&langchoice=it&stno=2-630-0-7671-0-0-2073-2055">Italiano</option><option value="https://www.pornhublive.com/login.php?AFNO=1-5457&g=L3NpZ251cC8%2FQUZOTz0xLTU0NTcmc3Rubz0yLTYzMC0wLTc2NzEtMC0wLTIwNzMtMjA1NQ%3D%3D&langchoice=nl&stno=2-630-0-7671-0-0-2073-2055">Nederlands</option><option value="https://www.pornhublive.com/login.php?AFNO=1-5457&g=L3NpZ251cC8%2FQUZOTz0xLTU0NTcmc3Rubz0yLTYzMC0wLTc2NzEtMC0wLTIwNzMtMjA1NQ%3D%3D&langchoice=pt&stno=2-630-0-7671-0-0-2073-2055">Português</option><option value="https://www.pornhublive.com/login.php?AFNO=1-5457&g=L3NpZ251cC8%2FQUZOTz0xLTU0NTcmc3Rubz0yLTYzMC0wLTc2NzEtMC0wLTIwNzMtMjA1NQ%3D%3D&langchoice=sv&stno=2-630-0-7671-0-0-2073-2055">Svenska</option>
+  </select>
+</li>
+
+    </ul>
+  </div>
+
+</footer>
+<div class="legal__copy">
+  <a rel="external" href="https://www.pornhublive.com/2257.html?AFNO=1-5457&stno=2-630-0-7671-0-0-2073-2055">18 U.S.C. 2257 and 2257A Record-Keeping Requirements Compliance Statement</a>
+</div>
+
+<img width="1" height="1" alt="" border="0" id="naiad_jp">
+<noscript>
+  <img class="hide" src="//a.air2s.com/v1/info/pixel?smrid=75615933594049890013820126441793296533-1584263777-158785441&smtid=401985e5-2355-46c4-8af5-a1cf787cc667G0111584263777&smeid=79bc2fc9-6c69-4a95-b0b3-627e97e3edb9G0111584263777&href=https%3A%2F%2Fwww.pornhublive.com%2Flogin.php%3FAFNO%3D1-5457%26g%3DL3NpZ251cC8%252FQUZOTz0xLTU0NTcmc3Rubz0yLTYzMC0wLTc2NzEtMC0wLTIwNzMtMjA1NQ%253D%253D%26stno%3D2-630-0-7671-0-0-2073-2055&referrer=https%3A%2F%2Fwww.pornhublive.com%2Flogin.php%3FAFNO%3D1-5457%26g%3DL3NpZ251cC8%252FQUZOTz0xLTU0NTcmc3Rubz0yLTYzMC0wLTc2NzEtMC0wLTIwNzMtMjA1NQ%253D%253D%26stno%3D2-630-0-7671-0-0-2073-2055" alt="pixeltracker" />
+</noscript>
+<script type="text/javascript" src="//ga.nsimg.net/icf-analytics.js?smrid=75615933594049890013820126441793296533-1584263777-158785441" integrity="sha384-tNOYgw5i4AePWqIeSedWaD2HVjJ+e5aMr63Y1uG8mv2FW5yuFtAW7NOQd5sKbhBi" crossorigin="anonymous"></script>
+	<script type="application/ld+json">
+	  {
+		  "@context": "http://schema.org",
+		  "@type": "WebSite",
+		  "url": "https://www.pornhublive.com",
+		  "name": "PornHubLive",
+		  "potentialAction": {
+		    "@type": "SearchAction",
+		    "target": "https://www.pornhublive.com/search.php?q={search_term_string}",
+		    "query-input": "required name=search_term_string"
+		  }
+	  }
+	</script>
+
+
+<noscript>
+  <img class="hide" src="https://www.pornhublive.com/pixel/nojs.php" alt="nojstracking">
+</noscript>
+<script src="//m2.nsimg.net/cache/js/bundle/naiad/20151203/naiad.js" ></script>
+<script>
+Naiad.Init({mediabase:'https://m1.nsimg.net',state:'stno=2-630-0-7671-0-0-2073-2055&AFNO=1-5457',wwwbase:location.protocol+'//'+location.hostname});
+
+Naiad.OnDomReadyQueue([ 50, function() {
+  Naiad.Util.SearchAnythingChecker( 'Search Anything', '.search-form', '#search-anything');
+  var naiad_jp = Naiad.Y.one('#naiad_jp');
+  if( naiad_jp )
+  {
+    naiad_jp.set("src", Naiad.GetFooterPixel());
+  }
+}, '' ], [ 60, function() {
+  Naiad.Util.InitClearNotice( '.public-note' );
+}, '' ]);
+
+Naiad.OnLoadQueue([ 0, function() {
+  setTimeout(function () {
+    var is404 = !!Naiad.Y.one('#error-page');
+    Naiad.LogPerformanceTiming(is404);
+  }, 0);
+}, '' ]);
+</script>
+
+
+<script type="text/javascript">
+  (function($) {
+    function hideFullSite() {
+      var iOS = ( navigator.userAgent.match(/(iPad|iPhone|iPod)/i) ? true : false );
+
+      if( iOS ) {
+        $('.full-site').hide();
+      }
+    }
+
+    function tierToggle()
+    {
+      var bIsActive,
+          sTarget = 'div .' + $(this).attr('data-target'),
+          sTargetTier = $(this).parents('.tier').next('.tier');
+
+      $(this).siblings('a[data-toggle=true]').removeClass('active');
+      $(this).toggleClass('active');
+
+      bIsActive = $(this).is('.active');
+
+      $(sTargetTier).toggleClass('active-tier', bIsActive );
+
+      $(sTarget).hide();
+      $(sTarget).siblings('a[data-toggle=true]').hide();
+      $(sTarget).toggle( bIsActive );
+    }
+
+    function goToURL()
+    {   
+      var url = $(this).val();  
+      
+      if (url) {  
+        window.location.assign(url);  
+      }
+         
+      return false;   
+    }
+
+    $(document).on('pageinit', function () {
+      hideFullSite();
+      $('a[data-toggle=true]').on('click', tierToggle);
+      $('select[data-autosubmit=true]').on('change', goToURL);
+      
+      if ($('#app_banner') && $('#download_overlay').length == 0)
+      {
+          $("div[data-role='page']").after("");
+      }
+      
+      $('#app_banner').on('click', function (e) {
+        var href;
+
+        href = $(this).attr('href');
+        if ((href != null) && ((href.indexOf('type=2') >= 0) || (href.indexOf('type=3') >= 0)))
+        {
+          return;
+        }
+
+        $.mobile.changePage($('#download_overlay'));
+        $("div[data-role='page']").css('display', 'block');
+      });
+    });
+    
+  })(jqm);
+</script>
+
+
+
+
+  </div>
+
+  
+  
+  
+  
+  
+
+  <script type="text/javascript" src="//m1.nsimg.net/static/x/yui/2.7.0b/build/yahoo/yahoo-min.js"></script>
+<script type="text/javascript" src="//m1.nsimg.net/static/x/yui/2.7.0b/build/cookie/cookie-min.js"></script>
+<script type="text/javascript" src="//m1.nsimg.net/static/x/yui/2.7.0b/build/dom/dom-min.js"></script>
+<script type="text/javascript" src="//m2.nsimg.net/static/x/yui/2.7.0b/build/event/event-min.js"></script>
+<script type="text/javascript">
+YAHOO.util.Event.onDOMReady(function(){
+  var ckst = YAHOO.util.Cookie.get("SMuicST");
+  if(ckst != null)
+  {
+    if(ckst.substring(0,3) == 'uep')
+    {
+      YAHOO.util.Dom.getElementsBy(function (e){return YAHOO.util.Dom.getAttribute(e, 'name') == 'sausr';}, 'input', 0, function (e){e.value=decodeURIComponent(ckst.substring(3));} );
+    }
+  }
+  ck = YAHOO.util.Cookie.get("SMuic");
+  if(ck != null)
+  {
+    if(ck.substring(0,3) == 'uep')
+    {
+      if(ckst == null)
+      {
+        YAHOO.util.Dom.getElementsBy(function (e){return YAHOO.util.Dom.getAttribute(e, 'name') == 'sausr';}, 'input', 0, function (e){e.value=decodeURIComponent(ck.substring(3));} );
+      }
+      YAHOO.util.Dom.getElementsBy(function (e){return YAHOO.util.Dom.getAttribute(e, 'name') == 'recognize';}, 'input', 0, function (e){YAHOO.util.Dom.setAttribute(e, 'checked', 1);} );
+    }
+  }
+}); 
+
+</script>
+
+
+  
+
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,160 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Create Your Account</title>
+<link href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" rel="stylesheet">
+<link href="https://secure.puba.com/shared/css/prejoin.css" rel="stylesheet">
+
+<style type="text/css">
+.join_error { color: #EE0000; }
+</style>
+
+
+<script type="text/javascript">
+if (document.all || document.getElementById){ //if IE4 or NS6+
+document.write('<style type="text/css">')
+document.write('.dyncontent{display:none;}')
+document.write('</style>')
+}
+</script>
+
+</head>
+
+<body>
+	<div class="header">
+		Get instant access to Asa Akira today! Create your account below.
+	</div>
+<div class="container">
+	
+<!-- right side graphic -->	
+	
+<div class="award">
+		<img src="https://secure.puba.com/shared/img/prejoin_puba.jpg" width="100%" />
+	</div>
+
+<!-- Create Your Account -->
+
+	<form action="signup.php" method="POST" data-ajax="false" target="_top">
+	<input type="hidden" name="nats" value="MobilePH.16.15.18.0.0.0.0.0">
+	<input type="hidden" name="step" value="signup">
+	<input type="hidden" name="username" value="">
+	<input type="hidden" name="signup[rename_old_member]" value="1">
+	<input type="hidden" name="signup[mailok]" value=1 checked>
+    
+
+			
+<div class="form-style-10">
+<h1>Create Your Account!<span>Choose your login info and membership option</span></h1>
+
+    <div class="section"><span>1</span>Create Login</div>
+    <div class="inner-wrap">
+				<input type="text" name="signup[username,email:1:1:128:::email_check]" id="un" value="" placeholder="Enter Your Email Address">
+		<div style="height:15px"></div>
+		        <input type="password" name="signup[password:1:6:16:::password_check]" id="pw" value="" placeholder="Create Password">
+    </div>
+	
+	<p class="dyncontent">	
+
+		<select class="join_select" name="signup[country]" width="200">	<option value='AF' >Afghanistan	<option value='AX' >Aland Islands	<option value='DZ' >Algeria	<option value='AS' >American Samoa	<option value='AD' >Andorra	<option value='AO' >Angola	<option value='AI' >Anguilla	<option value='AQ' >Antarctica	<option value='AG' >Antigua And Barbuda	<option value='AR' >Argentina	<option value='AM' >Armenia	<option value='AW' >Aruba	<option value='AU' >Australia	<option value='AT' >Austria	<option value='AZ' >Azerbaijan	<option value='BS' >Bahamas	<option value='BH' >Bahrain	<option value='BD' >Bangladesh	<option value='BB' >Barbados	<option value='BE' >Belgium	<option value='BZ' >Belize	<option value='BJ' >Benin	<option value='BM' >Bermuda	<option value='BT' >Bhutan	<option value='BO' >Bolivia	<option value='BQ' >Bonaire, Saint Eustatius and Saba	<option value='BW' >Botswana	<option value='BV' >Bouvet Island	<option value='BR' >Brazil	<option value='IO' >British Indian Ocean	<option value='BN' >Brunei Darussalam	<option value='BF' >Burkina Faso	<option value='BI' >Burundi	<option value='KH' >Cambodia	<option value='CM' >Cameroon	<option value='CA' >Canada	<option value='CV' >Cape Verde	<option value='KY' >Cayman Islands	<option value='CF' >Central African Republic	<option value='TD' >Chad	<option value='CL' >Chile	<option value='CN' >China	<option value='CX' >Christmas Island	<option value='CC' >Cocos (Keeling) Islands	<option value='CO' >Colombia	<option value='KM' >Comoros	<option value='CG' >Congo, Republic of the	<option value='CD' >Congo, Democratic Republic of the	<option value='CK' >Cook Islands	<option value='CR' >Costa Rica	<option value='CW' >Curacao	<option value='CY' >Cyprus	<option value='CZ' >Czech Republic	<option value='DK' >Denmark	<option value='DJ' >Djibouti	<option value='DM' >Dominica	<option value='DO' >Dominican Republic	<option value='EC' >Ecuador	<option value='EG' >Egypt	<option value='SV' >El Salvador	<option value='GQ' >Equatorial Guinea	<option value='ER' >Eritrea	<option value='EE' >Estonia	<option value='ET' >Ethiopia	<option value='FK' >Falkland Islands (Malvinas)	<option value='FO' >Faroe Islands	<option value='FJ' >Fiji	<option value='FI' >Finland	<option value='FR' >France	<option value='GF' >French Guiana	<option value='PF' >French Polynesia	<option value='TF' >French Southern Territories	<option value='GA' >Gabon	<option value='GM' >Gambia	<option value='GE' >Georgia	<option value='DE' >Germany	<option value='GH' >Ghana	<option value='GI' >Gibraltar	<option value='GR' >Greece	<option value='GL' >Greenland	<option value='GD' >Grenada	<option value='GP' >Guadeloupe	<option value='GU' >Guam	<option value='GT' >Guatemala	<option value='GG' >Guernsey	<option value='GN' >Guinea	<option value='GW' >Guinea-bissau	<option value='GY' >Guyana	<option value='HT' >Haiti	<option value='HM' >Heard Island and McDonald Islands	<option value='VA' >Holy See (Vatican City State)	<option value='HN' >Honduras	<option value='HK' >Hong Kong	<option value='HU' >Hungary	<option value='IS' >Iceland	<option value='IN' >India	<option value='ID' >Indonesia	<option value='IE' >Ireland	<option value='IM' >Isle of Man	<option value='IL' >Israel	<option value='IT' >Italy	<option value='JM' >Jamaica	<option value='JP' >Japan	<option value='JE' >Jersey	<option value='JO' >Jordan	<option value='KZ' >Kazakhstan	<option value='KE' >Kenya	<option value='KI' >Kiribati	<option value='KP' >Korea, Democratic People's Republic of	<option value='KR' >Korea, Republic of	<option value='KW' >Kuwait	<option value='KG' >Kyrgyzstan	<option value='LA' >Lao People's Democratic Republic	<option value='LV' >Latvia	<option value='LS' >Lesotho	<option value='LY' >Libya	<option value='LI' >Liechtenstein	<option value='LT' >Lithuania	<option value='LU' >Luxembourg	<option value='MO' >Macao	<option value='MG' >Madagascar	<option value='MW' >Malawi	<option value='MY' >Malaysia	<option value='MV' >Maldives	<option value='ML' >Mali	<option value='MT' >Malta	<option value='MH' >Marshall Islands	<option value='MQ' >Martinique	<option value='MR' >Mauritania	<option value='MU' >Mauritius	<option value='YT' >Mayotte	<option value='MX' >Mexico	<option value='FM' >Micronesia, Federated States of	<option value='MC' >Monaco	<option value='MN' >Mongolia	<option value='MS' >Montserrat	<option value='MA' >Morocco	<option value='MZ' >Mozambique	<option value='MM' >Myanmar	<option value='NA' >Namibia	<option value='NR' >Nauru	<option value='NP' >Nepal	<option value='NL' >Netherlands	<option value='AN' >Netherlands Antilles	<option value='NC' >New Caledonia	<option value='NZ' >New Zealand	<option value='NI' >Nicaragua	<option value='NE' >Niger	<option value='NG' >Nigeria	<option value='NU' >Niue	<option value='NF' >Norfolk Island	<option value='MP' >Northern Mariana Islands	<option value='NO' >Norway	<option value='OM' >Oman	<option value='PK' >Pakistan	<option value='PW' >Palau	<option value='PS' >Palestine, State of	<option value='PA' >Panama	<option value='PG' >Papua New Guinea	<option value='PY' >Paraguay	<option value='PE' >Peru	<option value='PH' >Philippines	<option value='PN' >Pitcairn	<option value='PL' >Poland	<option value='PT' >Portugal	<option value='PR' >Puerto Rico	<option value='QA' >Qatar	<option value='RE' >Reunion	<option value='RU' >Russian Federation	<option value='RW' >Rwanda	<option value='BL' >Saint Bartelemey	<option value='SH' >Saint Helena	<option value='KN' >Saint Kitts And Nevis	<option value='LC' >Saint Lucia	<option value='PM' >Saint Pierre And Miquelon	<option value='VC' >Saint Vincent and the Grenadines	<option value='MF' >Saint-Martin (French part)	<option value='WS' >Samoa	<option value='SM' >San Marino	<option value='ST' >Sao Tome and Principe	<option value='SA' >Saudi Arabia	<option value='SN' >Senegal	<option value='SC' >Seychelles	<option value='SL' >Sierra Leone	<option value='SG' >Singapore	<option value='SX' >Sint Maarten (Dutch part)	<option value='SK' >Slovakia	<option value='SI' >Slovenia	<option value='SB' >Solomon Islands	<option value='ZA' >South Africa	<option value='GS' >South Georgia and the South Sandwich...	<option value='SS' >South Sudan	<option value='ES' >Spain	<option value='LK' >Sri Lanka	<option value='SR' >Suriname	<option value='SJ' >Svalbard and Jan Mayen Islands	<option value='SZ' >Swaziland	<option value='SE' >Sweden	<option value='CH' >Switzerland	<option value='SY' >Syrian Arab Republic	<option value='TW' >Taiwan	<option value='TJ' >Tajikistan	<option value='TZ' >Tanzania, United Republic Of	<option value='TH' >Thailand	<option value='TL' >Timor-Leste	<option value='TG' >Togo	<option value='TK' >Tokelau	<option value='TO' >Tonga	<option value='TT' >Trinidad And Tobago	<option value='TN' >Tunisia	<option value='TR' >Turkey	<option value='TM' >Turkmenistan	<option value='TC' >Turks and Caicos Islands	<option value='TV' >Tuvalu	<option value='UG' >Uganda	<option value='UA' >Ukraine	<option value='AE' >United Arab Emirates	<option value='GB' >United Kingdom	<option value='US' selected>United States	<option value='UM' >United States Minor Outlying Islands	<option value='UY' >Uruguay	<option value='UZ' >Uzbekistan	<option value='VU' >Vanuatu	<option value='VE' >Venezuela	<option value='VN' >Vietnam	<option value='VG' >Virgin Islands (British)	<option value='VI' >Virgin Islands (US)	<option value='WF' >Wallis and Futuna Islands	<option value='EH' >Western Sahara	<option value='YE' >Yemen	<option value='ZM' >Zambia</select>
+
+	<label><input type="radio" name="cascade" value="4" checked="checked" />Join by Credit Card</label><br>
+	</p>
+	
+    <div class="section"><span>2</span>Choose Membership</div>
+    <div class="inner-wrap">
+		
+			
+			
+		
+		
+		
+		
+		<div class="best">Best Value</div>		 <input type="radio" class="hidden" name="signup[optionid]" id="signup_optionid_101" value="101" checked >
+		 <label for="signup_optionid_101">
+			  <div class="memText">
+				  <span class="optionBig">90 Day Membership</span>
+			  	  <div class="optionSmall">Full Access for 3 Months</div>
+		      </div>
+			      <div class="priceBox">$0.56/day</div>
+				  <div style="clear:both"></div>
+		  </label>
+		  
+		  
+		 
+  
+		  	
+		
+		
+		
+		
+				 <input type="radio" class="hidden" name="signup[optionid]" id="signup_optionid_139" value="139"  >
+		 <label for="signup_optionid_139">
+			  <div class="memText">
+				  <span class="optionBig">30 Day Membership</span>
+			  	  <div class="optionSmall">Full Access for 1 Month</div>
+		      </div>
+			      <div class="priceBox">$29.97</div>
+				  <div style="clear:both"></div>
+		  </label>
+		  
+		  
+		 
+  
+		  	
+		
+		
+		
+		
+				 <input type="radio" class="hidden" name="signup[optionid]" id="signup_optionid_138" value="138"  >
+		 <label for="signup_optionid_138">
+			  <div class="memText">
+				  <span class="optionBig">5 Day Membership</span>
+			  	  <div class="optionSmall">Easy Upgrade, Cancel Any Time</div>
+		      </div>
+			      <div class="priceBox">$5.00</div>
+				  <div style="clear:both"></div>
+		  </label>
+		  
+		  
+		 
+  
+		  		
+	
+		
+    </div>
+    <div class="section"><span>3</span>Create Account</div>
+        <div class="inner-wrap">
+			<div align="center">
+				<button type="submit">Create Your Account</button>
+
+	    </div>
+    </div>
+</form>
+
+<!-- Membership Bennefits -->
+
+<div class="bennefits">
+<div class="title">Membership Bennifits</div>
+<ul>
+	<li class="fas fa-film"><span class="secureText">Exclusive HD Content</span></li>
+	<li class="fas fa-calendar-alt"><span class="secureText">Daily Content Updates</span></li>
+	<li class="fas fa-mobile-alt"><span class="secureText"> Mobile & tablet Friendly</span></li>
+	<li class="fas fa-check-square"><span class="secureText">Full 1080p HD Videos</span></li>
+</ul>
+</div>
+<div class="secure">
+<div class="title">Always Safe & Secure</div>
+<ul>
+	<li class="fas fa-credit-card"><span class="secureText">Discrete Billing</span></li>
+	<li class="fas fa-smile"><span class="secureText">Top Notch Support</span></li><br>
+	<li class="fas fa-lock"><span class="secureText"> SSL Security</span></li><br>
+	<li class="fas fa-stop-circle"><span class="secureText">Cancel Anytime</span></li>
+</ul>
+</div>
+</div>
+
+</div><!-- End Container -->
+</body>
+</html>
